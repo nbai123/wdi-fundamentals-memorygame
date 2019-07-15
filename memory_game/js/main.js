396a6c0ef076103735f2d031815dedf754bc2193
Nick Bai <nick@nick.com>
2019-07-15 00:21:05 -0700

    created new gmae logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -32,23 +32,31 @@ function checkForMatch (){
 	}
 }
 
-function flipCard (cardId) {
+function flipCard () {
+	var cardId = this.getAttribute('data-id');
 // 	cardsInPlay.push(cardOne);
 // 	cardsInPlay.push(cardTwo);
 
-// if (cardsInPlay.length === 2) {
-// 	if (cardOne === cardTwo) {
-// 		alert("You found a match!");
-// 	}
-// 		else {
-// 			alert("Sorry, try again.");
-// 		}
-// 	}
 	console.log("User flipped " + cards[cardId].rank);
+
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 }
-flipCard (0);
-flipCard (2);
+}
+
+function createBoard (){
+for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', [i]);
+    cardElement.addEventListener ('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+}
+
+createBoard ();
+

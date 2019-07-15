8e289349d4432fb986f63e4239d4624ceb840abd
Nick Bai <nick@nick.com>
2019-07-14 22:39:12 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
 console.log('Up and running!');
-var cardOne = 'queen';
-var cardTwo = 'king';
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 var cardThree = 'queen';
 var cardFour = 'king';
 console.log('User Flipped ' + cardOne);
-console.log('User Flipped ' + cardFour);
+console.log('User Flipped ' + cardTwo);
+var cardsInPlay = [ ];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	}
+		else {
+			alert("Sorry, try again.");
+		}
+
+	}
+

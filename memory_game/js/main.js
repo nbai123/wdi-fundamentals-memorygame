da05211a77bbff2152bc006008b16d9c02db4e36
Nick Bai <nick@nick.com>
2019-07-14 23:06:30 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,32 @@
 console.log('Up and running!');
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-var cardThree = 'queen';
-var cardFour = 'king';
-console.log('User Flipped ' + cardOne);
-console.log('User Flipped ' + cardTwo);
 var cardsInPlay = [ ];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
-	}
-		else {
-			alert("Sorry, try again.");
-		}
 
+function checkForMatch (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+	} 
+	else {
+  console.log("Sorry, try again.");
 	}
+}
 
+function flipCard (cardId) {
+// 	cardsInPlay.push(cardOne);
+// 	cardsInPlay.push(cardTwo);
+
+// if (cardsInPlay.length === 2) {
+// 	if (cardOne === cardTwo) {
+// 		alert("You found a match!");
+// 	}
+// 		else {
+// 			alert("Sorry, try again.");
+// 		}
+// 	}
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+flipCard (0);
+flipCard (2);
